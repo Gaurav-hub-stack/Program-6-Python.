d5550c8b21764753bbda181ec2585dac1f642f44
Gaurav-hub-stack <115688930+Gaurav-hub-stack@users.noreply.github.com>
2023-10-20 23:13:23 +0530

    Add files via upload

---
+++ p4 datatype/new1.py
@@ -0,0 +1,34 @@
+#datatype
+#answers following 2 questions
+#1. Operations on values
+#2. Memory usage
+#Commonly used standard datatypes
+#int
+ int a = 10
+print("a")
+#float
+ float y =23.2
+print("y  ")
+#complex-->
+x=5+7j
+print(type(x),x)
+x=7j
+print(x)
+#none is value nonetype data type 
+x=None
+print(x)
+print(none==23)
+#string
+#4 way to represent string
+>"................"
+>'................'
+>""" ............"""
+>'''.............'''
+
+#using operation
++,*,.<,>=,==,!=,in,etc.
+#usiing functions
+ len,min.max,sum.ord,chr,etc.
+#using methods
+upper,lower,title,waspcase,capitalize
+strip,lstrip,rstrip,replace,index,count
